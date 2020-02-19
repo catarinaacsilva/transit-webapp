70ecd3f281b28523ec948738e1d502266b6367f8
Catarina Silva <c.alexandracorreia@ua.pt>
2020-02-19 23:24:56 +0000

    Removed test file

--- transit/src/main/java/ua/es/transit/Test.java
+++
@@ -1,9 +0,0 @@
-package ua.es.transit;
-
-import java.util.List;
-
-public class Test {
-    public static void main(String[] args) {
-        List<Incident> incidents = IncidentService.getList(new GeoPoint(0,0), new GeoPoint(1,1));
-    }
-}
