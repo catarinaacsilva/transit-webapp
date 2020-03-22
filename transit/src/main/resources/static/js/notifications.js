2a9f69094f292912c1e71d552697c0a443b54957
Catarina Silva <c.alexandracorreia@ua.pt>
2020-03-22 22:18:16 +0000

    Finish project

--- transit/src/main/resources/static/js/notifications.js
+++ transit/src/main/resources/static/js/notifications.js
@@ -1,3 +1,5 @@
+var lista = [];
+
 $( window ).ready(function() {
   connect();
 });
@@ -7,7 +9,15 @@ function connect() {
   stompClient = Stomp.over(socket);
   stompClient.connect({}, function (frame) {
       stompClient.subscribe('/topic/delay', function (notification) {
-          $('#textArea').val(notification);
+          var obj = JSON.parse(notification.body);
+          lista.push(obj);
+          document.getElementById("no").style.display = "none";
+
+          var node = document.createElement("li");
+          var textnode = document.createTextNode("Vendor: " + obj["vendor"] + ", Type: " + obj["type"] + ", Line: " + obj["line"] + " (" + obj["begin"] + " <-> " + obj["end"] + "), Delay: " + obj["delay"] + " min");
+          node.appendChild(textnode);
+          node.className = "list-group-item"
+          document.getElementById("del").insertBefore(node, document.getElementById("del").childNodes[0]);
        });
   });
 }
